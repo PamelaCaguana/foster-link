6120abfa1a523ae4c4527c336494ed76f1e4614f
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-02 23:06:24 -0600

    add GET routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const homeController = require('../controllers/home')
 
 //@description  Login/Landing Page
 //@route        GET /
 //add middleware as a second argument
-router.get('/', homeController.getIndex)
+router.get('/', (req, res) => {
+    res.render('login', {
+        layout: 'login',
+    })
+})
+
+//@description  Login/Landing Page
+//@route        GET /dashboard
+//add middleware as a second argument
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
 
 module.exports = router
