48bd70b051bae10fe831e8a66e73df8504239886
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-05 21:20:13 -0600

    add posts.js file

---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+//@description  Auth
+//@route        GET /
+//add middleware as a second argument
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+//@description  Login/Landing Page
+//@route        GET /dashboard
+//add middleware as a second argument
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+module.exports = router
